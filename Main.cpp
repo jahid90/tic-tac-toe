540eb84e744b33d2038d29f16eca51b28abfb33a
Jahidul Islam Sarkar <s.jahid.90@gmail.com>
2013-09-23 23:29:41 +0530

    Added class TicTacToe
    Added a class, declared some methods and implemented the constructor and
    the printBoard() method.

--- Main.cpp
+++ Main.cpp
@@ -3,8 +3,59 @@
 
 using namespace std;
 
+class TicTacToe {
+private:
+	bool finished;
+	int MAXROW;
+	int MAXCOL;
+	char **board;
+public:
+	// constructors and destructors
+	TicTacToe();
+
+	// onject methods
+	void printBoard();
+	void playerOneMove (int x, int y);
+	void playerTwoMove (int x, int y);
+};
+
+TicTacToe::TicTacToe () {
+	finished = false;
+	MAXROW = 3;
+	MAXCOL = 3;
+	board = new char*[MAXROW];
+	for (int i = 0; i < MAXROW; ++i) {
+		board[i] = new char[MAXCOL];
+		for (int j = 0; j < MAXCOL; ++j) {
+			board[i][j] = '#';
+		}
+	}
+}
+
+void TicTacToe::printBoard () {
+	cout << endl;
+	for (int i = 0; i < MAXROW; ++i) {
+		for (int j = 0; j < MAXCOL; ++j) {
+			cout << board[i][j] << " ";
+		}
+		cout << endl;
+	}
+}
+
+void TicTacToe::playerOneMove (int x, int y) {
+
+}
+
+void TicTacToe::playerTwoMove (int x, int y) {
+
+}
+
 int main (int argc, char **argv) {
-	cout << "TIC-TAC-TOE" << endl;
+	cout << "*******TIC-TAC-TOE*******" << endl;
+
+	TicTacToe game;
+
+	game.printBoard();
 	
 	return EXIT_SUCCESS;
 }
