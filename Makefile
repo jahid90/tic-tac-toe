f71d3424166e1509bb603c2ba251cda8f84cf9b9
Jahid <s.jahid.90@gmail.com>
2016-01-03 15:48:39 +0530

    wipe the slate clean

--- Makefile
+++
@@ -1,5 +0,0 @@
-TicTacToe: *.cpp *.h
-	g++ $(CFLAGS) -o TicTacToe *.cpp
-	
-clean:
-	rm -f *.o *.exe
--- header.h
+++
@@ -1,7 +0,0 @@
-#ifndef __HEADER_H__
-#define __HEADER_H__
-
-//#include "Player.h"
-//#include "Engine.h"
-
-#endif  // __HEADER_H__
